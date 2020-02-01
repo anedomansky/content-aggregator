1d4850b5c2bfe32120089eb6cda09c6f4a70538c
Alexander Nedomansky <anedomansky@googlemail.com>
2020-02-01 10:31:27 +0100

    Worked on the GamingPage.

--- frontend/src/components/common/List.tsx
+++ frontend/src/components/common/List.tsx
@@ -10,14 +10,17 @@ interface Props {
 const List: React.FC<Props> = ({ title, items }) => (
     <article className="list">
         <h1 className="list__title">{title}</h1>
-        <ul>
-            {items.map((item: IPage, index: number) => (
-                <li key={index}>
-                    <a href={item.link} target="_blank" rel="noopener noreferrer">
-                        <span>{item.snippet}</span>
-                    </a>
-                </li>
-            ))}
+        <ul className="list__items">
+            {items.length === 0
+                ? (<span>No items to display...</span>)
+                : items.map((item: IPage, index: number) => (
+                    <li key={index}>
+                        <a href={item.link} target="_blank" rel="noopener noreferrer">
+                            <span>{item.snippet}</span>
+                        </a>
+                        <hr />
+                    </li>
+                ))}
         </ul>
     </article>
 );
--- frontend/src/components/gaming/GamingPage.tsx
+++ frontend/src/components/gaming/GamingPage.tsx
@@ -5,10 +5,12 @@ import './GamingPage.scss';
 import { IStories } from '../../interfaces/IStories';
 import { IPage } from '../../interfaces/IPage';
 
+// TODO: fetch new stories onClick
 const GamingPage: React.FC = () => {
     const [vg247News, setVg247News] = useState<IPage[]>([]);
     const [gamerantNews, setGamerantNews] = useState<IPage[]>([]);
     const [gameinformerNews, setGameinformerNews] = useState<IPage[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
     const fetchNews = async (): Promise<IStories> => {
         const responseRaw = await fetch('http://localhost:5000/gaming/all');
@@ -21,14 +23,21 @@ const GamingPage: React.FC = () => {
         setVg247News(news.stories.vg247);
         setGamerantNews(news.stories.gamerant);
         setGameinformerNews(news.stories.gameinformer);
+        setLoading(false);
     };
 
     useEffect(() => {
         setNews();
     }, []);
 
+    if (loading) {
+        return (
+            <article className="gaming-pagel--loading">Loading news. Please wait...</article>
+        );
+    }
     return (
         <article className="gaming-page">
+            <button type="button" className="gaming-page__fetch-btn" onClick={() => console.log('Get new stories!')}>Fetch new stories?</button>
             <div className="gaming-page__first-page">
                 <List title="VG247" items={vg247News} />
             </div>
